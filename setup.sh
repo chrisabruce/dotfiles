d607d3aefb282668146fa84e5294bd92653eb7bd
Chris Bruce <chris@chrisbruce.com>
2020-01-23 13:55:40 +0100

    adds basic setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Vim Plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Symlink
+ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+
