6d5bcc7451b4a31dcb140c654b3f414e285c402f
Chris Bruce <chris@chrisbruce.com>
2020-01-25 14:03:41 +0100

    updates setup to sym tmux

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.g
 
 # Symlink
 ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
