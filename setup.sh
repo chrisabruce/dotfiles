054d05fdb7a3570d0358c1432455dac0a169cb90
Chris Bruce <chris@chrisbruce.com>
2020-01-25 15:14:44 +0100

    adds alacritty

--- setup.sh
+++ setup.sh
@@ -3,7 +3,12 @@
 # Install Vim Plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/alacritty
+
 # Symlink
 ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
 
